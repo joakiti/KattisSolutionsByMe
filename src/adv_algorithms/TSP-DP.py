e919a119177d004e74b8b2a367a900cdff4bfe37
mikke <mikkel_ander@hotmail.com>
2020-11-09 21:29:36 +0100

    update TSP-DP.py

--- src/adv_algorithms/TSP-DP.py
+++ src/adv_algorithms/TSP-DP.py
@@ -1,42 +1,57 @@
 import itertools
-import math
 import sys
 from collections import defaultdict
-
+from sys import maxsize
+from itertools import permutations
+from haversine import haversine, Unit
 from dijkstar import Graph, find_path
 import operator
-
-v = 5
-
-vertices = [i for i in range(0, v)]
 
 graph = Graph()
 
 
 def addEdge(frm, to, cost):
     graph.add_edge(frm, to, cost)
-    graph.add_edge(to, frm, cost)
 
 
-for v in vertices:
-    for neighbor in range(v, len(vertices)):
-        addEdge(v, neighbor, 1)
+cities = dict()
+index = 0
+while sys.stdin:
+    try:
+        i = input()
+        name, lat, lon = i.split()
+        cities[index] = (name, lat, lon)
+        index += 1
+    except:
+        break
 
-OPT = defaultdict(defaultdict)
+for city in cities.keys():
+    name, lat, lon = cities[city]
+    for other in cities.keys():
+        if city != other:
+            neig, lat2, lon2 = cities[other]
+            weight = haversine((float(lat), float(lon)), (float(lat2), float(lon2)))
+            graph.add_edge(city, other, weight)
 
-for t in range(0, len(vertices)):
-    OPT[t][tuple({t})] = find_path(graph, 0, t).total_cost
+start = 0
+DP = defaultdict(defaultdict)
+DP[tuple([start]), start] = 0
+vertex = list(filter(lambda v: v != start, cities.keys()))
 
+for size in range(1, len(cities.keys())):
+    print(size)
+    for S in itertools.combinations(vertex, size):
+        for end in S:
+            visited = tuple([start]) + tuple(S)
+            if size < 2:
+                DP[visited][end] = find_path(graph, start, end).total_cost
+            else:
+                removeEnd = tuple(filter(lambda v: v != end, visited))
+                DP[visited][end] = min([DP[removeEnd][j] + find_path(graph, j, end).total_cost for j in S if j != end and j != 0])
 
-for t in range(1, len(vertices)):
-    for size in range(1, len(vertices)):
-        for S in itertools.combinations(vertices, size):
-            minCost = sys.maxsize
-            endRemoved = set(filter(lambda vtx: vtx != t, S))
-            for x in endRemoved:
-                minCost = min(minCost, OPT[x][tuple(endRemoved)] + find_path(graph, x, t).total_cost)
-            OPT[t][tuple({S})] = minCost
+minimal = sys.maxsize
+for t in cities.keys():
+    if t != 0:
+        minimal = min(minimal, DP[tuple(cities.keys())][t] + find_path(graph, t, start).total_cost)
 
-
-
-
+print(minimal)
