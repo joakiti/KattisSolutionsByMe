473cbdd7ef99bae1fde14a094462681ff399b2bf
mikke <mikkel_ander@hotmail.com>
2020-11-12 11:29:17 +0100

    update TSP-DP.py

--- src/adv_algorithms/TSP-DP.py
+++ src/adv_algorithms/TSP-DP.py
@@ -7,13 +7,6 @@ from haversine import haversine, Unit
 from dijkstar import Graph, find_path
 import operator
 
-graph = Graph()
-
-
-def addEdge(frm, to, cost):
-    graph.add_edge(frm, to, cost)
-
-
 cities = dict()
 index = 0
 while sys.stdin:
@@ -24,6 +17,7 @@ while sys.stdin:
         index += 1
     except:
         break
+edges = defaultdict(defaultdict)
 
 for city in cities.keys():
     name, lat, lon = cities[city]
@@ -31,7 +25,8 @@ for city in cities.keys():
         if city != other:
             neig, lat2, lon2 = cities[other]
             weight = haversine((float(lat), float(lon)), (float(lat2), float(lon2)))
-            graph.add_edge(city, other, weight)
+            edges[city][other] = weight
+
 
 start = 0
 DP = defaultdict(defaultdict)
@@ -39,19 +34,18 @@ DP[tuple([start]), start] = 0
 vertex = list(filter(lambda v: v != start, cities.keys()))
 
 for size in range(1, len(cities.keys())):
-    print(size)
     for S in itertools.combinations(vertex, size):
         for end in S:
             visited = tuple([start]) + tuple(S)
             if size < 2:
-                DP[visited][end] = find_path(graph, start, end).total_cost
+                DP[visited][end] = edges[start][end]
             else:
                 removeEnd = tuple(filter(lambda v: v != end, visited))
-                DP[visited][end] = min([DP[removeEnd][j] + find_path(graph, j, end).total_cost for j in S if j != end and j != 0])
+                DP[visited][end] = min([DP[removeEnd][j] + edges[j][end] for j in S if j != end and j != 0])
 
 minimal = sys.maxsize
 for t in cities.keys():
     if t != 0:
-        minimal = min(minimal, DP[tuple(cities.keys())][t] + find_path(graph, t, start).total_cost)
+        minimal = min(minimal, DP[tuple(cities.keys())][t] + edges[t][start])
 
 print(minimal)
