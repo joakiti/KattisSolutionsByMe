6c62a44ef4b21b9383e3e7fceab61c80a42936dc
mikke <mikkel_ander@hotmail.com>
2020-11-25 21:46:55 +0100

    give possibility of multiple variables

--- src/adv_algorithms/experiments/QuadraticTableLookupFourSum.java
+++ src/adv_algorithms/experiments/QuadraticTableLookupFourSum.java
@@ -4,16 +4,9 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class QuadraticTableLookupFourSum {
-    static String test = "5\n" +
-            "4\n" +
-            "-3\n" +
-            "2\n" +
-            "1\n" +
-            "-7";
 
     public static void main(String[] args) {
-        HashMap<Long, HashMap<Long, Long>> H = new HashMap<>();
-        HashMap<Long, HashMap<Integer, Integer>> H2 = new HashMap<>();
+        HashMap<Long, HashMap<Integer, ArrayList<Integer>>> H2 = new HashMap<>();
 
         Scanner S = new Scanner(System.in);
 
@@ -23,34 +16,36 @@ public class QuadraticTableLookupFourSum {
             vals[i] = Long.parseLong(S.nextLine());
         }
         for (int i = 0; i < N; i++) {
-            H.putIfAbsent(vals[i], new HashMap<>());
             for (int j = i + 1; j < N; j++) {
-                H.get(vals[i]).put(vals[j], vals[i] + vals[j]);
                 H2.putIfAbsent(vals[i] + vals[j], new HashMap<>());
-                H2.get(vals[i] + vals[j]).put(j, i);
+                H2.get(vals[i] + vals[j]).putIfAbsent(j, new ArrayList<>());
+                H2.get(vals[i] + vals[j]).get(j).add(i);
             }
         }
         // the variables are ordered l < k < j < i; sorry
         for (int i = 0; i < N; ++i)
             for (int j = i + 1; j < N; j += 1) {// i goes through {0, ..., N-1}
-                Long key = H.get(vals[i]).get(vals[j]);
+                long key = vals[i] + vals[j];
                 int finalI = i;
                 int finalJ = j;
-                if (H2.containsKey(-key)) {
-                    var solution =  H2.get(-key)
-                            .entrySet()
-                            .stream()
-                            .filter(integerIntegerEntry ->
-                                    integerIntegerEntry.getKey() != finalI &&
-                                            integerIntegerEntry.getKey() != finalJ &&
-                                            integerIntegerEntry.getValue() != finalJ &&
-                                            integerIntegerEntry.getValue() != finalI ).findAny();
-                    if (solution.isPresent()) {
+                var solutionKey = H2.get(-key);
+                if (solutionKey != null) {
+                    checkIfUniqueSolution(i, j, solutionKey);
+                }
+            }
+        System.out.println("None");
+    }
+
+    private static void checkIfUniqueSolution(int i, int j, HashMap<Integer, ArrayList<Integer>> solutionKey) {
+        for (Map.Entry<Integer, ArrayList<Integer>> values : solutionKey.entrySet()) {
+            if (values.getKey() != i && values.getKey() != j) {
+                for (Integer possibleMatch : values.getValue()) {
+                    if (possibleMatch != i && possibleMatch != j) {
                         System.out.println("Found");
                         System.exit(0);
                     }
                 }
             }
-        System.out.println("None");
+        }
     }
 }
