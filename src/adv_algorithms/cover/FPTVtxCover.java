420f0580f6302a8c4c86c044b89dd1c9c29817d5
mikke <mikkel_ander@hotmail.com>
2020-11-26 10:49:14 +0100

    Fixed typo from graph 1 to graph 2..

--- src/adv_algorithms/cover/FPTVtxCover.java
+++ src/adv_algorithms/cover/FPTVtxCover.java
@@ -2,26 +2,37 @@ package adv_algorithms.cover;
 
 import Codeforces_654.F;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class FPTVtxCover {
 
     public static void main(String[] args) {
         HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
 
-        for (int i = 0; i < 3; i++) {
+        /**
+         * Testing
+        for (int i = 0; i < 24; i++) {
             graph.putIfAbsent(i, new ArrayList<>());
             graph.get(i).add(i + 1);
             graph.putIfAbsent(i + 1, new ArrayList<>());
             graph.get(i + 1).add(i);
+        }
+        **/
+        Scanner s = new Scanner(System.in);
 
+        graph = new HashMap<>();
+        while (s.hasNext()) {
+            int from = s.nextInt();
+            int to = s.nextInt();
+            graph.putIfAbsent(from, new ArrayList<>());
+            graph.putIfAbsent(to, new ArrayList<>());
+            graph.get(from).add(to);
+            graph.get(to).add(from);
         }
 
         FPTVtxCover alg = new FPTVtxCover();
-        System.out.println(alg.run(graph, 2, new ArrayList<>()));
+        System.out.println(alg.run(graph, 12, new ArrayList<>()));
     }
 
 
@@ -51,7 +62,7 @@ public class FPTVtxCover {
 
 
         ans1 = run(G1, k-1, ans1);
-        ans2 = run(G1, k-1, ans2);
+        ans2 = run(G2, k-1, ans2);
 
         if (ans1.isEmpty()) {
             return ans2;
@@ -64,7 +75,7 @@ public class FPTVtxCover {
     void removeVtx(HashMap<Integer, ArrayList<Integer>> graph, int vtx) {
         for (int neighbour : graph.get(vtx)) {
             graph.get(neighbour).remove((Object)vtx);
-            if (graph.get(neighbour).size() == 0) {
+            if (graph.get(neighbour).isEmpty()) {
                 graph.remove(neighbour);
             }
         }
